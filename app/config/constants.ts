4b174d99537a1f03f0452f6d3e972dcc63b72379
Todd Appleton <toddappleton@dreamfactory.com>
2016-08-30 12:39:24 -0400

    Added package file

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,2 +1,2 @@
-export const DSP_API_KEY: string = 'd4ffe12c1602e2f0aa7c2ac9338c634295fc7e8dcd9ea0291013cbf26271dfc1';
-export const DSP_INSTANCE_URL: string = 'https://ft-ravindra.vz2.dreamfactory.com';
+export const DSP_API_KEY: string = '762a0cc3c2d085db706b897c4d9fc02ca9ee96a4fae8663e207824f44cd904c5';
+export const DSP_INSTANCE_URL: string = 'http://localhost:8888';
