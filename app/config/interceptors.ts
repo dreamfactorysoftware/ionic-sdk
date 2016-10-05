307e1f02d21b4e6f613a01c1cc95abef9feb6e80
Javascript Boss <javascriptboss99@gmail.com>
2016-10-06 00:14:28 +0530

    Session issues

--- app/config/interceptors.ts
+++ app/config/interceptors.ts
@@ -9,8 +9,8 @@ export class DfRequestOptions extends BaseRequestOptions {
 		this.headers.set('X-Dreamfactory-API-Key' ,constants.DSP_API_KEY);
 
 		var token = localStorage.getItem('session_token');
-		if (token) {
-			this.headers.set('X-Dreamfactory-Session-Token', token);
-		}
+		// if (token) {
+		// 	this.headers.set('X-Dreamfactory-Session-Token', token);
+		// }
 	}
 }
